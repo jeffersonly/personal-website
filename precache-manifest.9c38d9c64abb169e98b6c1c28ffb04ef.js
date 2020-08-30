f0d2f14b9cf1d579e9062046f27452042e065ca7
Jefferson Ly <32202953+jeffersonly@users.noreply.github.com>
2020-08-29 19:18:39 -0700

    Updates

---
+++ precache-manifest.9c38d9c64abb169e98b6c1c28ffb04ef.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682608c85d26e2ee39580edc3bf73cf1",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "7b9aaa9df49f0c62f746",
+    "url": "/portfolio-website/static/css/main.bd56963a.chunk.css"
+  },
+  {
+    "revision": "9838ad6a7c52def7ff78",
+    "url": "/portfolio-website/static/js/2.1a45c695.chunk.js"
+  },
+  {
+    "revision": "86a2181252fd016547a137056c8c4e10",
+    "url": "/portfolio-website/static/js/2.1a45c695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9aaa9df49f0c62f746",
+    "url": "/portfolio-website/static/js/main.e5361975.chunk.js"
+  },
+  {
+    "revision": "68c7102406ff3bd0c5bc",
+    "url": "/portfolio-website/static/js/runtime-main.3b1c52a1.js"
+  },
+  {
+    "revision": "4e6a80b0aa3669de4e411f81a72bc374",
+    "url": "/portfolio-website/static/media/Resume.4e6a80b0.pdf"
+  },
+  {
+    "revision": "2167fed1580f162e68099f448d1db74d",
+    "url": "/portfolio-website/static/media/cloud1.2167fed1.png"
+  },
+  {
+    "revision": "41d1e7792a2728b629cc6364de045279",
+    "url": "/portfolio-website/static/media/cloud2.41d1e779.png"
+  },
+  {
+    "revision": "7c878e086b8ba8d11993fd2cc1754379",
+    "url": "/portfolio-website/static/media/cloud3.7c878e08.png"
+  },
+  {
+    "revision": "ea046c5c1b3407aab18460846eafe93f",
+    "url": "/portfolio-website/static/media/cloud4.ea046c5c.png"
+  },
+  {
+    "revision": "34ae4c87d746dcf1f2e9fcb5fe049180",
+    "url": "/portfolio-website/static/media/cloud5.34ae4c87.png"
+  },
+  {
+    "revision": "7916575f2b7fbe6f1403b0a697d7b066",
+    "url": "/portfolio-website/static/media/contact.7916575f.jpg"
+  },
+  {
+    "revision": "bb4ec9deb0dc5acb0237a4fb27f0b9ab",
+    "url": "/portfolio-website/static/media/contact.bb4ec9de.svg"
+  },
+  {
+    "revision": "33528cf5fa1459f29aaadfc74db13550",
+    "url": "/portfolio-website/static/media/darkexperiencepage.33528cf5.png"
+  },
+  {
+    "revision": "797592ff195d5011a06d9833ca012060",
+    "url": "/portfolio-website/static/media/darkhomepage.797592ff.jpg"
+  },
+  {
+    "revision": "39ac34fb87149ffbd9397677e34b39ca",
+    "url": "/portfolio-website/static/media/darkprojectspage.39ac34fb.jpg"
+  },
+  {
+    "revision": "69c7ed4dab81c8a3f3107ad3e8105aa1",
+    "url": "/portfolio-website/static/media/home.69c7ed4d.svg"
+  },
+  {
+    "revision": "2c0b54645750fd258d35372cf3e386c7",
+    "url": "/portfolio-website/static/media/lightexperiencepage.2c0b5464.jpg"
+  },
+  {
+    "revision": "c16393790bed827c097eb59992c9f16c",
+    "url": "/portfolio-website/static/media/lighthomepage.c1639379.jpg"
+  },
+  {
+    "revision": "09d7545eadbbc5cb160e4eb36f4be5bc",
+    "url": "/portfolio-website/static/media/lightprojectspage.09d7545e.jpg"
+  },
+  {
+    "revision": "a62240937f6159b6a7e2090919e49356",
+    "url": "/portfolio-website/static/media/moon.a6224093.svg"
+  },
+  {
+    "revision": "d6b173e429d599d247756426976b487e",
+    "url": "/portfolio-website/static/media/resume.d6b173e4.svg"
+  },
+  {
+    "revision": "696d1dd7dd611cff0e0d9926ca519bd2",
+    "url": "/portfolio-website/static/media/sun.696d1dd7.svg"
+  },
+  {
+    "revision": "1eb06c16e0b10510be8a5034980dd921",
+    "url": "/portfolio-website/static/media/work.1eb06c16.svg"
+  }
+]);
